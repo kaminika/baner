7349a22547a99095fddc9997cfc0bd609b78e7f5
kaminika <klaudia.k1998@wp.pl>
2024-10-16 08:17:28 +0200

    rozpoczecie

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const baner = document.getElementsByClassName("banner");
+
+console.log(baner);
+        
+/* banerScroll {
+    baner.addEventListener("wheel", () => {
+        contentFull[i].classList.remove("scroll");
+    })
+} */
+
+baner.classList.add("hidden");
